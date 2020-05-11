ece4550b33d8ac7c21110b782975a6580b21d574
ac2pic <ac2pic@gmail.com>
2020-05-11 15:52:52 -0500

    Cleaned up stage-helper script

--- js/game/stage-helper.js
+++ js/game/stage-helper.js
@@ -1,5 +1,5 @@
 
-(async function() {
+(async function () {
 
     Object.freeze(window.preload);
     Object.freeze(window.postload);
@@ -7,7 +7,7 @@
     const Plugin = window.Plugin;
     Object.freeze(window.mods);
 
-    for(const mod of mods) {
+    for (const mod of mods) {
         if (mod.plugin) {
             const instance = await import(mod.plugin);
             const pluginClass = instance && instance.default;
@@ -16,26 +16,43 @@
                 if (pluginInstance instanceof Plugin) {
                     mod.setPlugin(pluginInstance);
                 }
-            } 
-        }  
+            }
+        }
     }
 
 
+    function loadGame() {
+        const deferredNodes = Array.from(document.querySelectorAll('script[type="javascript/wait-for-preload"]'));
+        for (const node of deferredNodes) {
+            const script = document.createElement('script');
+            for (const attrib of node.attributes) {
+                script.setAttributeNode(attrib.cloneNode());
+            }
+            script.setAttribute('type', 'text/javascript');
+            document.head.appendChild(script);
+        }
+    }
 
+    // preload 
     preload().then(() => loadGame());
 
-    let ig = undefined;
-    
     function setupPostloadStage(ig, oldFunc) {
-        ig._DOMReady = function() {
+        ig._DOMReady = function () {
             postload().then(() => oldFunc())
         };
     }
+
+    callWhenIgSet(function (ig) {
+        // postload
+        setupPostloadStage(ig, ig._DOMReady);
+    });
+
 
     let startCrossCode = undefined;
     Object.defineProperty(window, 'startCrossCode', {
         set(value) {
             startCrossCode = value;
+            // prestart
             prestart().then(() => startCrossCode());
         },
         get() {
@@ -43,35 +60,5 @@
         }
     });
 
-    const igCallbacks = [];
-    
-    window.callWhenIgSet = function(func) {
-        igCallbacks.push(func);
-    }
 
-    Object.defineProperty(window, 'ig', {
-        set(value) {
-            if (ig !== value) {
-                ig = value;
-                igCallbacks.forEach(e => e(value));
-                setupPostloadStage(ig, ig._DOMReady);
-            }
-        },
-        get() {
-            return ig;
-        }
-    });
-
-
-    function loadGame() {
-        const deferredNodes = Array.from(document.querySelectorAll('script[type="javascript/wait-for-preload"]'));
-        for (const node of deferredNodes) {
-            const script = document.createElement('script');
-            for(const attrib of node.attributes) {
-                script.setAttributeNode(attrib.cloneNode());
-            }
-            script.setAttribute('type', 'text/javascript');
-            document.head.appendChild(script);
-        }
-    }
 })()
