a92c60a79b2447e4d1dc93cf3ef6595f65863bae
ac2pic <ac2pic@gmail.com>
2020-07-02 13:17:24 -0500

    Removed fs.js

--- js/mod-fs.js
+++ js/mod-fs.js
@@ -1,8 +1,7 @@
-import Fs from "./fs.js";
-
-export default class ModFs extends Fs {
+export default class ModFs {
     constructor(baseDirectory) {
-        super();
+        this.fs = this.isNw() ? require('fs') : null;
+        this.path = this.isNw() ? require('path') : null;
         this.baseDirectory = baseDirectory;
     }
 
@@ -44,5 +43,31 @@ export default class ModFs extends Fs {
             }
         }
         return files;
+    }
+    
+    async getModPaths() {
+        const mods = [];
+
+        if (this.isNw()) {
+            const modsFolder = this.path.join(process.cwd(), 'assets/', 'mods');
+            if (this.fs.existsSync(modsFolder)) {
+                const modsFolders = this.fs.readdirSync(modsFolder);
+                for (const modFolder of modsFolders) {
+                    let modExists = this.fs.existsSync(this.path.join(modsFolder, modFolder, 'package.json'));;
+                    if (modExists) {
+                        mods.push(location.origin + '/assets/mods/' + modFolder);
+                    }
+                }
+            }
+
+        } else {
+
+        }
+
+        return mods;
+    }
+
+    isNw() {
+        return typeof nw !== 'undefined' && nw.require;
     }
 }
